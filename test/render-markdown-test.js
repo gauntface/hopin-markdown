dd2d5b26cb28a0fe7096b22b694e522dbdd39937
Matt Gaunt <mattgaunt@google.com>
2019-06-03 19:44:55 -0700

    Fixing test

--- test/render-markdown-test.js
+++ test/render-markdown-test.js
@@ -23,140 +23,117 @@ test('renderMarkdown() should throw for an object input', async (t) => {
 
 test('renderMarkdown() should render HTML p heading', async (t) => {
 	const render = await renderMarkdown('Hello World');
-  t.deepEqual(render.html, '<p>Hello World</p>');
-  t.deepEqual(render.tokens, ['p']);
+  t.deepEqual(render, '<p>Hello World</p>');
 });
 
 test('renderMarkdown() should render HTML h1 heading', async (t) => {
 	const render = await renderMarkdown('# Hello World');
-  t.deepEqual(render.html, '<h1 id="hello-world">Hello World</h1>');
-  t.deepEqual(render.tokens, ['h1']);
+  t.deepEqual(render, '<h1 id="hello-world">Hello World</h1>');
 });
 
 test('renderMarkdown() should render HTML h2 heading', async (t) => {
 	const render = await renderMarkdown('## Hello World');
-  t.deepEqual(render.html, '<h2 id="hello-world">Hello World</h2>');
-  t.deepEqual(render.tokens, ['h2']);
+  t.deepEqual(render, '<h2 id="hello-world">Hello World</h2>');
 });
 
 test('renderMarkdown() should render HTML h3 heading', async (t) => {
 	const render = await renderMarkdown('### Hello World');
-  t.deepEqual(render.html, '<h3 id="hello-world">Hello World</h3>');
-  t.deepEqual(render.tokens, ['h3']);
+  t.deepEqual(render, '<h3 id="hello-world">Hello World</h3>');
 });
 
 test('renderMarkdown() should render HTML h4 heading', async (t) => {
 	const render = await renderMarkdown('#### Hello World');
-  t.deepEqual(render.html, '<h4 id="hello-world">Hello World</h4>');
-  t.deepEqual(render.tokens, ['h4']);
+  t.deepEqual(render, '<h4 id="hello-world">Hello World</h4>');
 });
 
 test('renderMarkdown() should render HTML h5 heading', async (t) => {
 	const render = await renderMarkdown('##### Hello World');
-  t.deepEqual(render.html, '<h5 id="hello-world">Hello World</h5>');
-  t.deepEqual(render.tokens, ['h5']);
+  t.deepEqual(render, '<h5 id="hello-world">Hello World</h5>');
 });
 
 test('renderMarkdown() should render HTML h6 heading', async (t) => {
 	const render = await renderMarkdown('###### Hello World');
-  t.deepEqual(render.html, '<h6 id="hello-world">Hello World</h6>');
-  t.deepEqual(render.tokens, ['h6']);
+  t.deepEqual(render, '<h6 id="hello-world">Hello World</h6>');
 });
 
 test('renderMarkdown() should render inline code', async (t) => {
 	const render = await renderMarkdown('Hello `World`');
-  t.deepEqual(render.html, '<p>Hello <code>World</code></p>');
-  t.deepEqual(render.tokens, ['p', 'code'].sort());
+  t.deepEqual(render, '<p>Hello <code>World</code></p>');
 });
 
 test('renderMarkdown() should render inline italics', async (t) => {
 	const render = await renderMarkdown('Hello *World*');
-  t.deepEqual(render.html, '<p>Hello <em>World</em></p>');
-  t.deepEqual(render.tokens, ['p', 'em'].sort());
+  t.deepEqual(render, '<p>Hello <em>World</em></p>');
 });
 
 test('renderMarkdown() should render inline bold', async (t) => {
 	const render = await renderMarkdown('Hello **World**');
-  t.deepEqual(render.html, '<p>Hello <strong>World</strong></p>');
-  t.deepEqual(render.tokens, ['p', 'strong'].sort());
+  t.deepEqual(render, '<p>Hello <strong>World</strong></p>');
 });
 
 test('renderMarkdown() should render unordered list', async (t) => {
 	const render = await renderMarkdown('- Hello World');
-  t.deepEqual(render.html, '<ul>\n<li>Hello World</li>\n</ul>');
-  t.deepEqual(render.tokens, ['ul', 'li'].sort());
+  t.deepEqual(render, '<ul>\n<li>Hello World</li>\n</ul>');
 });
 
 test('renderMarkdown() should render ordered list', async (t) => {
 	const render = await renderMarkdown('1. Hello World');
-  t.deepEqual(render.html, '<ol>\n<li>Hello World</li>\n</ol>');
-  t.deepEqual(render.tokens, ['ol', 'li'].sort());
+  t.deepEqual(render, '<ol>\n<li>Hello World</li>\n</ol>');
 });
 
 test('renderMarkdown() should render plain code block', async (t) => {
 	const render = await renderMarkdown('```\nconsole.log(\'Hello World\');\n```');
-  t.deepEqual(render.html, '<pre><code>console.log(&#39;Hello World&#39;);</code></pre>');
-  t.deepEqual(render.tokens, ['pre', 'code'].sort());
+  t.deepEqual(render, '<pre><code>console.log(&#39;Hello World&#39;);</code></pre>');
 });
 
 test('renderMarkdown() should syntax highlight Javascript code block', async (t) => {
 	const render = await renderMarkdown('```javascript\nconsole.log(\'Hello World\');\n```');
-  t.deepEqual(render.html, '<pre><code class="language-javascript">console<span class="token punctuation">.</span><span class="token function">log</span><span class="token punctuation">(</span><span class="token string">\'Hello World\'</span><span class="token punctuation">)</span><span class="token punctuation">;</span></code></pre>');
-  t.deepEqual(render.tokens, ['pre', 'code', 'code-highlighted'].sort());
+  t.deepEqual(render, '<pre><code class="language-javascript">console<span class="token punctuation">.</span><span class="token function">log</span><span class="token punctuation">(</span><span class="token string">\'Hello World\'</span><span class="token punctuation">)</span><span class="token punctuation">;</span></code></pre>');
 });
 
 /* test('renderMarkdown() should syntax highlight Javascript code block with first line entry', async (t) => {
 	const render = await renderMarkdown('```\njavascript\nconsole.log(\'Hello World\');\n```');
-  t.deepEqual(render.html, '<pre><code class="language-javascript">console<span class="token punctuation">.</span><span class="token function">log</span><span class="token punctuation">(</span><span class="token string">\'Hello World\'</span><span class="token punctuation">)</span><span class="token punctuation">;</span></code></pre>');
-  t.deepEqual(render.tokens, ['pre', 'code', 'code-highlighted'].sort());
+  t.deepEqual(render, '<pre><code class="language-javascript">console<span class="token punctuation">.</span><span class="token function">log</span><span class="token punctuation">(</span><span class="token string">\'Hello World\'</span><span class="token punctuation">)</span><span class="token punctuation">;</span></code></pre>');
 });*/
 
 test('renderMarkdown() should render plain code block for unknown language', async (t) => {
 	const render = await renderMarkdown('```unknown\nconsole.log(\'Hello World\');\n```');
-  t.deepEqual(render.html, '<pre><code>console.log(&#39;Hello World&#39;);</code></pre>');
-  t.deepEqual(render.tokens, ['pre', 'code'].sort());
+  t.deepEqual(render, '<pre><code>console.log(&#39;Hello World&#39;);</code></pre>');
 });
 
 test('renderMarkdown() should render blockquote', async (t) => {
 	const render = await renderMarkdown('> Hello World');
-  t.deepEqual(render.html, '<blockquote>\n<p>Hello World</p>\n</blockquote>');
-  t.deepEqual(render.tokens, ['blockquote', 'p'].sort());
+  t.deepEqual(render, '<blockquote>\n<p>Hello World</p>\n</blockquote>');
 });
 
 test('renderMarkdown() should render horizontal rule', async (t) => {
 	const render = await renderMarkdown('---');
-  t.deepEqual(render.html, '<hr>');
-  t.deepEqual(render.tokens, ['hr'].sort());
+  t.deepEqual(render, '<hr>');
 });
 
 test('renderMarkdown() should render html', async (t) => {
 	const render = await renderMarkdown('Hello World\n<div>This is HTML</div>');
-  t.deepEqual(render.html, '<p>Hello World</p>\n<div>This is HTML</div>');
-  t.deepEqual(render.tokens, ['p', 'div'].sort());
+  t.deepEqual(render, '<p>Hello World</p>\n<div>This is HTML</div>');
 });
 
 test('renderMarkdown() should render br', async (t) => {
 	const render = await renderMarkdown('Hello  \nWorld');
-  t.deepEqual(render.html, '<p>Hello<br>World</p>');
-  t.deepEqual(render.tokens, ['p', 'br'].sort());
+  t.deepEqual(render, '<p>Hello<br>World</p>');
 });
 
 test('renderMarkdown() should render del', async (t) => {
 	const render = await renderMarkdown('Hello ~~Matt~~World');
-  t.deepEqual(render.html, '<p>Hello <del>Matt</del>World</p>');
-  t.deepEqual(render.tokens, ['p', 'del'].sort());
+  t.deepEqual(render, '<p>Hello <del>Matt</del>World</p>');
 });
 
 test('renderMarkdown() should render link', async (t) => {
 	const render = await renderMarkdown('[Hello World](https://gauntface.com/)');
-  t.deepEqual(render.html, '<p><a href="https://gauntface.com/">Hello World</a></p>');
-  t.deepEqual(render.tokens, ['p', 'a'].sort());
+  t.deepEqual(render, '<p><a href="https://gauntface.com/">Hello World</a></p>');
 });
 
 test('renderMarkdown() should render link with title', async (t) => {
 	const render = await renderMarkdown('[Hello World](https://gauntface.com/ "Gauntface Home")');
-  t.deepEqual(render.html, '<p><a href="https://gauntface.com/" title="Gauntface Home">Hello World</a></p>');
-  t.deepEqual(render.tokens, ['p', 'a'].sort());
+  t.deepEqual(render, '<p><a href="https://gauntface.com/" title="Gauntface Home">Hello World</a></p>');
 });
 
 test('renderMarkdown() should render basic table', async (t) => {
@@ -164,8 +141,7 @@ test('renderMarkdown() should render basic table', async (t) => {
 | Hello World |
 | ----------- |
   `);
-  t.deepEqual(render.html, '<table>\n<thead>\n<tr>\n<th>Hello World</th>\n</tr>\n</thead>\n</table>');
-  t.deepEqual(render.tokens, ['table', 'thead', 'th', 'tr'].sort());
+  t.deepEqual(render, '<table>\n<thead>\n<tr>\n<th>Hello World</th>\n</tr>\n</thead>\n</table>');
 });
 
 test('renderMarkdown() should render full table', async (t) => {
@@ -174,42 +150,36 @@ test('renderMarkdown() should render full table', async (t) => {
 | ------ |
 | World  |
   `);
-  t.deepEqual(render.html, '<table>\n<thead>\n<tr>\n<th>Hello</th>\n</tr>\n</thead>\n<tbody><tr>\n<td>World</td>\n</tr>\n</tbody></table>');
-  t.deepEqual(render.tokens, ['table', 'tbody', 'thead', 'th', 'tr', 'td'].sort());
+  t.deepEqual(render, '<table>\n<thead>\n<tr>\n<th>Hello</th>\n</tr>\n</thead>\n<tbody><tr>\n<td>World</td>\n</tr>\n</tbody></table>');
 });
 
 test('renderMarkdown() should render image', async (t) => {
   const render = await renderMarkdown(`![Alt Text](./images/example.png)`);
-  t.deepEqual(render.html, '<p><img src="./images/example.png" alt="Alt Text"></p>');
-  t.deepEqual(render.tokens, ['p', 'img'].sort());
+  t.deepEqual(render, '<p><img src="./images/example.png" alt="Alt Text"></p>');
 });
 
 test('renderMarkdown() should render HTML img with gif as data-src', async (t) => {
 	const render = await renderMarkdown(`![Alt Text](./images/example.gif)`);
-  t.deepEqual(render.html, '<p><img data-src="./images/example.gif" alt="Alt Text"></p>');
-  t.deepEqual(render.tokens, ['p', 'img', 'async-img'].sort());
+  t.deepEqual(render, '<p><img data-src="./images/example.gif" alt="Alt Text"></p>');
 });
 
 test('renderMarkdown() should render image with http:// at start as plain image', async (t) => {
   const render = await renderMarkdown(`![Alt Text](http://example.com/images/example.png)`);
-  t.deepEqual(render.html, '<p><img src="http://example.com/images/example.png" alt="Alt Text"></p>');
-  t.deepEqual(render.tokens, ['p', 'img'].sort());
+  t.deepEqual(render, '<p><img src="http://example.com/images/example.png" alt="Alt Text"></p>');
 });
 
 test('renderMarkdown() should render image as src set if image is available', async (t) => {
   const render = await renderMarkdown(`![Alt Text](/picture-sets/basic.jpg)`, {
     staticDir: path.join(__dirname, 'static'),
   });
-  t.deepEqual(render.html, '<p><picture><source srcset="/picture-sets/basic.jpg/1.webp 1w, /picture-sets/basic.jpg/2.webp 2w" type="image/webp"><source srcset="/picture-sets/basic.jpg/1.jpg 1w, /picture-sets/basic.jpg/2.jpg 2w"><img src="/picture-sets/basic.jpg/2.jpg" alt="Alt Text" /></picture></p>');
-  t.deepEqual(render.tokens, ['p', 'img', 'picture'].sort());
+  t.deepEqual(render, '<p><picture><source srcset="/picture-sets/basic.jpg/1.webp 1w, /picture-sets/basic.jpg/2.webp 2w" type="image/webp"><source srcset="/picture-sets/basic.jpg/1.jpg 1w, /picture-sets/basic.jpg/2.jpg 2w"><img src="/picture-sets/basic.jpg/2.jpg" alt="Alt Text" /></picture></p>');
 });
 
 test('renderMarkdown() should handle the image being a file instead of a directory', async (t) => {
   const render = await renderMarkdown(`![Alt Text](/picture-sets/non-directory-image.jpg)`, {
     staticDir: path.join(__dirname, 'static'),
   });
-  t.deepEqual(render.html, '<p><img src="/picture-sets/non-directory-image.jpg" alt="Alt Text"></p>');
-  t.deepEqual(render.tokens, ['p', 'img'].sort());
+  t.deepEqual(render, '<p><img src="/picture-sets/non-directory-image.jpg" alt="Alt Text"></p>');
 });
 
 const sandbox = sinon.createSandbox();
@@ -229,6 +199,5 @@ test.serial('renderMarkdown() should render plain code block is prism throws an 
   });
 
   const render = await renderMarkdown('```javascript\nconsole.log(\'Hello World\');\n```');
-  t.deepEqual(render.html, '<pre><code>console.log(&#39;Hello World&#39;);</code></pre>');
-  t.deepEqual(render.tokens, ['pre', 'code'].sort());
+  t.deepEqual(render, '<pre><code>console.log(&#39;Hello World&#39;);</code></pre>');
 });
